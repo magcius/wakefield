4b2dc5c803de220c05e5fd83f00b1807ea406421
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2021-06-23 13:00:01 -0300

    License as LGPL v2
    Use LGPL instead of GPL to allow reusing the component in non GPL libraries like Gtk.

--- wakefield-compositor.c
+++ wakefield-compositor.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2015 Endless Mobile
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
+ * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  *
  * Written by:
  *     Jasper St. Pierre <jstpierre@mecheye.net>
@@ -223,7 +223,7 @@ bind_output (struct wl_client *client,
   struct wl_resource *cr;
 
   cr = wl_resource_create (client, &wl_output_interface, version, id);
-  wl_resource_set_destructor (cr, unbind_resource);
+ // wl_resource_set_destructor (cr, unbind_resource);
   /* wl_list_insert (&priv->output_resources, cr); */
   wl_output_send_scale (cr, gtk_widget_get_scale_factor (GTK_WIDGET (compositor)));
 }
